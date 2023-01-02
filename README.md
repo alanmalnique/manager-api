01b3f5765d6b41a8194fcd784b60b4867b06e1d8
unknown <alanmalnique@outlook.com>
2023-01-02 13:38:41 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Manager - API
+This document describes how to install and configure the Manager API on your local environment.
---
+++ tests/lib/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\App;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function test_FooBar(): void
+    {
+        $this->assertTrue(true);
+    }
+}
